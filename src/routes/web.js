5b3d563d2c8ec4681ad7759fe7e12cab52b921ba
tam2110097 <nguyenminhtam301103@gmail.com>
2025-01-09 22:44:18 +0700

    video 24

---
+++ src/routes/web.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+  res.send('Hello World! & nodemon')
+})
+
+router.get('/abc', (req, res) => {
+  res.send('check ABC')
+})
+
+router.get('/hoidanit', (req, res) => {
+  // res.send('<h1>Nguyen Minh Tam</h1>')
+  res.render('sample.ejs')  
+})
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,36 +1,15 @@
-const express = require('express')
-const path = require('path')
-require('dotenv').config()
-// import express from 'express';
-
+require('dotenv').config();
+const express = require('express');
+const configViewEngine = require('./config/viewEngine');
+const webRoutes = require('./routes/web');
 const app = express();
 const port = process.env.PORT ||8888;
-const hostname = process.env.HOST_NAME;
+const hostname = process.env.HOST_NAME; 
 
-// config template engine
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+//config template engine
+configViewEngine(app);
 
-//config static files
-app.use(express.static(path.join(__dirname, 'public')))
-
-//khai báo route
-app.get('/', (req, res) => {
-  res.send('Hello World! & nodemon')
-})
-
-//khai báo route
-app.get('/abc', (req, res) => {
-  res.send('check ABC')
-})
-
-//khai báo route
-app.get('/hoidanit', (req, res) => {
-  // res.send('<h1>Nguyen Minh Tam</h1>')
-  res.render('sample.ejs')  
-})
-
-
+app.use('/', webRoutes);
 
 app.listen(port, hostname,   () => {
   console.log(`Example app listening on port ${port}`)

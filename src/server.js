a4445c54c04d957e380f03a46b79fc2466da6dd9
tam2110097 <nguyenminhtam301103@gmail.com>
2025-01-09 13:11:49 +0700

    video 19

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const path = require('path')
+require('dotenv').config()
+// import express from 'express';
+console.log('>>>>>>>>>check env', process.env)
+
+const app = express()
+const port = process.env.PORT ||8888;
+const hostname = process.env.HOST_NAME;
+
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+//khai báo route
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+//khai báo route
+app.get('/abc', (req, res) => {
+  res.send('check ABC')
+})
+
+//khai báo route
+app.get('/hoidanit', (req, res) => {
+  // res.send('<h1>Nguyen Minh Tam</h1>')
+  res.render('sample.ejs')  
+})
+
+
+
+app.listen(port, hostname,   () => {
+  console.log(`Example app listening on port ${port}`)
+})

59ae977a72fa8a4f6f9e5db8a80f06790bd5915c
tam2110097 <nguyenminhtam301103@gmail.com>
2025-01-15 15:17:24 +0700

    video 33

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,21 @@
+const connection = require('../config/database');
+
 const getHomepage = (req, res) => {
-    res.send('Hello World! & nodemon')
+    //process data
+    //call model
+
+    let users = [];
+
+    connection.query(
+        'select * from Users u',
+        function (err, results, fields) {
+            users = results;
+            console.log(">>>>results= ", results); // results contains rows returned by server
+
+            console.log('>>>>check users: ', users)
+            res.send(JSON.stringify(users))
+        }
+    );
 }
 
 const getABC = (req, res) => {

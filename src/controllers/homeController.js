459dcd4b26f2cbf79b3ce65da773caa57b40f935
tam2110097 <nguyenminhtam301103@gmail.com>
2025-01-16 19:26:15 +0700

    video 38

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -15,8 +15,24 @@ const getHoidanit = (req, res) => {
 }
 
 const postCreateUser = (req, res) => {
-    console.log('>>>>>check req.body: ', req.body)
-    res.send('create a new user')
+
+    let email = req.body.email;
+    let name = req.body.myname;
+    let city = req.body.city;
+
+    console.log('>>>>> email = ', email, ' name =', name, ' city = ', city)
+
+    // let { email, name, city } = req.body;
+
+    connection.query(
+        `INSERT INTO 
+        Users (email, name, city) 
+        VALUES (?, ?, ?)`,
+        [email, name, city],
+        function (err, results) {
+            res.send('Create user succeed!')
+        }
+    );
 }
 
 module.exports = {
